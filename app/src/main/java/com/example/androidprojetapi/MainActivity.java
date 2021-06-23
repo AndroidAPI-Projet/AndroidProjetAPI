296814b72cf234e95102a7df285a5801daf81783
EvaBarbaro <barbaro.eva@outlook.fr>
2021-06-23 10:36:14 +0200

    Aerosoft login + menu

--- app/src/main/java/com/example/androidprojetapi/MainActivity.java
+++ app/src/main/java/com/example/androidprojetapi/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.androidprojetapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageAerosoft, imageMusicoshop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageAerosoft = (ImageView) findViewById(R.id.imageAerosoft);
+        imageMusicoshop = (ImageView) findViewById(R.id.imageMusicoshop);
+
+        imageAerosoft.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), LoginAerosoft.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
