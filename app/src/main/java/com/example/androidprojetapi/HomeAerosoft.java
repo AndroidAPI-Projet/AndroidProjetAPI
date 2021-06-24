f66f44af364580ba9bd6de057fecfc90e9f9b648
EvaBarbaro <barbaro.eva@outlook.fr>
2021-06-24 11:03:37 +0200

    HomePage

---
+++ app/src/main/java/com/example/androidprojetapi/HomeAerosoft.java
@@ -0,0 +1,14 @@
+package com.example.androidprojetapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeAerosoft extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_aerosoft);
+    }
+}
