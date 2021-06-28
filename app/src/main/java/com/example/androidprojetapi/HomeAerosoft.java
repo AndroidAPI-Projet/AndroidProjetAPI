982dc75b1725876c8bfb9d9ec8f991b694ed0fb0
EvaBarbaro <barbaro.eva@outlook.fr>
2021-06-28 16:03:51 +0200

    login ok

--- app/src/main/java/com/example/androidprojetapi/HomeAerosoft.java
+++ app/src/main/java/com/example/androidprojetapi/HomeAerosoft.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeAerosoft extends AppCompatActivity {
 
@@ -15,6 +16,14 @@ public class HomeAerosoft extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_aerosoft);
+
+        Intent intent = getIntent();
+
+        String message = intent.getStringExtra("message");
+
+        if(message != null) {
+            Toast.makeText(HomeAerosoft.this, message, Toast.LENGTH_LONG).show();
+        }
 
         piloteButton = (Button) findViewById(R.id.piloteButton);
         volButton = (Button) findViewById(R.id.volButton);
--- app/src/main/java/com/example/androidprojetapi/LoginAerosoft.java
+++ app/src/main/java/com/example/androidprojetapi/LoginAerosoft.java
@@ -4,12 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Properties;
 
 public class LoginAerosoft extends AppCompatActivity {
 
     Button btnRegisterAerosoft, btnLoginAerosoft;
+
+    EditText txtUsernameAerosoft, txtPasswordAerosoft;
+
+    private PropertyReader propertyReader;
+
+    private Properties properties;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +56,58 @@ public class LoginAerosoft extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), HomeAerosoft.class);
-                startActivity(intent);
+                Login();
             }
 
         });
+    }
+
+    private void Login() {
+
+        txtUsernameAerosoft = (EditText) findViewById(R.id.txtUsernameAerosoft);
+        txtPasswordAerosoft = (EditText) findViewById(R.id.txtPasswordAerosoft);
+
+        try {
+            propertyReader = new PropertyReader(this);
+            properties = propertyReader.getMyProperties("app.properties");
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String API_URL="http://"+ properties.getProperty("IP_Machine") +"/AerosoftAPI/utilisateur/login";
+            JSONObject jsonBody = new JSONObject();
+
+            jsonBody.put("Mail", txtUsernameAerosoft.getText().toString());
+            jsonBody.put("MotDePasse", txtPasswordAerosoft.getText().toString());
+            jsonBody.put("Statut", "true");
+
+            JsonObjectRequest stringRequest = new JsonObjectRequest (Request.Method.POST, API_URL, jsonBody, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        String message = response.getString("message");
+                        Intent intent = new Intent(LoginAerosoft.this, HomeAerosoft.class);
+                        intent.putExtra("message", message);
+                        startActivity(intent);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Mot de passe ou login erroné", Toast.LENGTH_SHORT).show();
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
